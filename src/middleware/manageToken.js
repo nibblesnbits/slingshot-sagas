47f29e29b6b0961d178b9f95ee131448233bcf4b
Stephen Collins <stephen.collins@centeva.com>
2016-09-20 15:33:53 -0400

    adding auth header to 'useToken' requests

--- src/middleware/manageToken.js
+++ src/middleware/manageToken.js
@@ -3,6 +3,18 @@ import * as keys from '../constants/storageKeys';
 
 export default function manageTokenMiddleware(store) {
   return (next) => (action) => {
+
+    // TODO: this feels hacky
+    if (action.useToken) {
+      const headers = action.config.headers || {};
+      action.config = {
+        headers: {
+          ...headers,
+          Authorization: `Bearer ${localStorage.getItem(keys.ACCESS_TOKEN)}`
+        }
+      }
+    }
+
     switch (action.type) {
       case types.CHECK_CREDS:
         const token = localStorage.getItem(keys.ACCESS_TOKEN);
@@ -11,9 +23,8 @@ export default function manageTokenMiddleware(store) {
         }
         return next(action);
       case types.LOGIN_SUCCESS:
-        const result = next(action);
-        localStorage.setItem(keys.ACCESS_TOKEN, result.access_token);
-        return result;
+        localStorage.setItem(keys.ACCESS_TOKEN, action.result.access_token);
+        return next(action);
       case types.LOGOUT_REQUEST:
         localStorage.removeItem(keys.ACCESS_TOKEN);
         return next(action);
