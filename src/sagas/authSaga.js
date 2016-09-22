a96478ab67f58678f58ada892e5a2b2b91662a4a
Stephen Collins <stephen.collins@centeva.com>
2016-09-22 17:48:29 -0400

    cut out some authSaga ceremony

--- src/sagas/authSaga.js
+++ src/sagas/authSaga.js
@@ -3,22 +3,22 @@ import { put } from 'redux-saga/effects';
 import callApi from './helpers';
 import * as types from '../constants/actionTypes';
 import * as appActions from '../actions/app';
-import { parseJwt } from '../util/jwtParser';
+// import { parseJwt } from '../util/jwtParser';
 
 export function* login({username, password}) {
 
   const config = {
     method: 'POST',
     headers: { 'Content-Type':'application/x-www-form-urlencoded' },
-    body: `grant_type=password&username=${username}&password=${password}&client_id=Centeva`
+    body: `grant_type=password&username=${username}&password=${password}&client_id=Redux`
   };
   const url = "http://localhost:3001/sessions/create";
 
   try {
-    const response = yield* callApi(url, config, [types.LOGIN_REQUEST_SUCCESS, types.LOGIN_REQUEST_FAILURE]);
-    const { payload } = parseJwt(response.access_token);
-    const username = payload.username;
-    yield put(appActions.showToast('Success!', `logged in as ${username}`,'success'));
+    /*const response =*/ yield* callApi(url, config, [types.LOGIN_REQUEST_SUCCESS, types.LOGIN_REQUEST_FAILURE]);
+    // const { payload } = parseJwt(response.access_token);
+    // const username = payload.username;
+    // yield put(appActions.showToast('Success!', `logged in as ${username}`,'success'));
   } catch(error) {
     const text = typeof(error) === "string" ? error : (typeof(error) === "object" && error.message ? error.message : "Unknown Error");
     yield put(appActions.showMessage('Login Error:', text, 'danger'));
