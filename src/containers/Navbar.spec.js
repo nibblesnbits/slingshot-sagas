b4ff42a504f5cd01afbc7b7e4608156d396bec8a
Stephen Collins <stephen.collins@centeva.com>
2016-09-22 18:04:24 -0400

    removed uncessary tests form Navbar spec

--- src/containers/Navbar.spec.js
+++ src/containers/Navbar.spec.js
@@ -55,48 +55,4 @@ describe('<Navbar />', () => {
 
     expect(loginButton.length).to.equal(1);
   });
-
-  it('should call login() on button click', () => {
-    const props = {
-      isAuthenticated: false,
-      logout: sinon.spy(),
-      login: sinon.spy(),
-      hideMessage: sinon.spy()
-    };
-
-    const wrapper = mount(
-      <Provider store={store}>
-          <Navbar {...props} />
-      </Provider>
-    );
-
-    const loginButton = wrapper.find('button[type="submit"]');
-
-    expect(loginButton.length).to.equal(1);
-    loginButton.simulate('click');
-
-    expect(props.login.calledOnce).to.equal(true);
-  });
-
-  it('should call logout() on button click', () => {
-    const props = {
-      isAuthenticated: true,
-      logout: sinon.spy(),
-      login: sinon.spy(),
-      hideMessage: sinon.spy()
-    };
-
-    const wrapper = mount(
-      <Provider store={store}>
-          <Navbar {...props} />
-      </Provider>
-    );
-
-    const loginButton = wrapper.find('.logout-button');
-
-    expect(loginButton.length).to.equal(1);
-    loginButton.simulate('click');
-
-    expect(props.logout.calledOnce).to.equal(true);
-  });
 });
