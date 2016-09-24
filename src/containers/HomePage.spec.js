2ebdd8bb3b52bfb6719f0ed0e6d115f3a8a0c6c1
Stephen Collins <stephen.collins@centeva.com>
2016-09-23 20:08:48 -0400

    added more HomePage tests

--- src/containers/HomePage.spec.js
+++ src/containers/HomePage.spec.js
@@ -32,4 +32,54 @@ describe('<HomePage />', () => {
 
     expect(props.fetchQuote.calledOnce).to.equal(true);
   });
+
+  it('should call fetchQuote() on quote button click()', () => {
+    const props = {
+      quote: 'test',
+      isAuthenticated: false,
+      isSecretQuote: false,
+      isFetching: false,
+      fetchQuote: sinon.spy(),
+      fetchSecretQuote: sinon.spy()
+    };
+    const store = createStore(rootReducer, initialState);
+
+    const wrapper = mount(
+      <Provider store={store}>
+          <HomePage {...props} />
+      </Provider>
+    );
+
+    const quoteButton = wrapper.find('.quote-button');
+    expect(quoteButton.length).to.equal(1);
+
+    quoteButton.simulate('click');
+
+    expect(props.fetchQuote.callCount).to.equal(2);
+  });
+
+  it('should call fetchSecretQuote() on secret quote button click()', () => {
+    const props = {
+      quote: 'test',
+      isAuthenticated: true,
+      isSecretQuote: false,
+      isFetching: false,
+      fetchQuote: sinon.spy(),
+      fetchSecretQuote: sinon.spy()
+    };
+    const store = createStore(rootReducer, initialState);
+
+    const wrapper = mount(
+      <Provider store={store}>
+          <HomePage {...props} />
+      </Provider>
+    );
+
+    const quoteButton = wrapper.find('.secret-quote-button');
+    expect(quoteButton.length).to.equal(1);
+
+    quoteButton.simulate('click');
+
+    expect(props.fetchSecretQuote.calledOnce).to.equal(true);
+  });
 });
