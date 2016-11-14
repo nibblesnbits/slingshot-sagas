a6fdc00c505d24b824da74aafc4182cef8da1aa6
Stephen Collins <stephen.collins@centeva.com>
2016-11-14 12:09:59 -0500

    fixed toastSaga tests

--- src/sagas/toastSaga.js
+++ src/sagas/toastSaga.js
@@ -3,11 +3,13 @@ import { put, call } from 'redux-saga/effects';
 import * as types from '../constants/actionTypes';
 import * as actions from '../actions/appActions';
 
+export const FADE_DELAY = 2500;
+
 export function* showToast({ title, text, className, duration }) {
   const message = yield put(actions.showMessage(title, text, className));
   yield call(delay, duration);
   yield put(actions.fadeMessage(message.id));
-  yield call(delay, 750);
+  yield call(delay, FADE_DELAY);
   yield put(actions.removeMessage(message.id));
 }
 
